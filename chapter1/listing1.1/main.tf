4b44c25704de345b36f328ed056ac5c0a9d862fa
thimotyb <thimoty@thimoty.it>
2024-01-30 17:23:41 +0100

    Updated ami code

--- chapter1/listing1.1/main.tf
+++ chapter1/listing1.1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "helloworld" {
-  ami           = "ami-09dd2e08d601bff67"
+  ami           = "ami-048ac6bc3c08c2f04"
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
