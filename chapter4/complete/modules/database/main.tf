b8ecb85d4e04ac8f8f65291cb14bff105b7520aa
thimotyb <thimoty@thimoty.it>
2024-10-22 15:35:31 +0200

    Update main.tf - change dbinstance to t3micro, reduced password chars on db

--- chapter4/complete/modules/database/main.tf
+++ chapter4/complete/modules/database/main.tf
@@ -1,14 +1,14 @@
 resource "random_password" "password" { #A
   length           = 16
   special          = true
-  override_special = "_%@/'\""
+  override_special = "@/"
 }
 
 resource "aws_db_instance" "database" {
   allocated_storage      = 10
   engine                 = "mysql"
   engine_version         = "8.0"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   identifier             = "${var.namespace}-db-instance"
   name                   = "pets"
   username               = "admin"
